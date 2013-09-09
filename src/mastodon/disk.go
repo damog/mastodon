8bf394b5299b9f18004690dfbdcccfdce3700a5e
Charles Leifer <coleifer@gmail.com>
2013-09-09 12:04:00 -0500

    Fixing tabs / removing semicolons

--- src/mastodon/disk.go
+++ src/mastodon/disk.go
@@ -4,9 +4,9 @@ import "syscall"
 
 func DiskUsage(path string) (free float64, total float64) {
     // Return bytes free and total bytes.
-    buf := new(syscall.Statfs_t);
-	syscall.Statfs("/", buf);
-	free = float64(buf.Bsize) * float64(buf.Bfree)
-	total = float64(buf.Bsize) * float64(buf.Blocks)
-	return
+    buf := new(syscall.Statfs_t)
+    syscall.Statfs("/", buf)
+    free = float64(buf.Bsize) * float64(buf.Bfree)
+    total = float64(buf.Bsize) * float64(buf.Blocks)
+    return
 }
